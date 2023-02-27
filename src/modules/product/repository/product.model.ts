93ec93c499566d6a0bbe6bb3d4f466db606defc0
Matheus Teixeira <matheus.teixeira7@gmail.com>
2023-02-27 17:35:10 -0300

    feat: add product model

---
+++ src/modules/product/repository/product.model.ts
@@ -0,0 +1,53 @@
+import { Column, Model, PrimaryKey, Table } from 'sequelize-typescript'
+
+@Table({
+  tableName: 'products',
+  timestamps: false
+})
+export class ProductModel extends Model {
+  @PrimaryKey
+  @Column({ allowNull: false })
+    id!: string
+
+  @Column({ allowNull: false })
+    category!: string
+
+  @Column({ allowNull: false })
+    subcategory!: string
+
+  @Column({ allowNull: false })
+    size!: string[]
+
+  @Column({ allowNull: false })
+    name!: string
+
+  @Column({ allowNull: false })
+    price!: number
+
+  @Column({ allowNull: false })
+    rating!: number
+
+  @Column({ allowNull: false })
+    images!: Array<{
+    id: string
+    name: string
+    src: string
+    alt: string
+  }>
+
+  @Column({ allowNull: false })
+    colors!: Array<{
+    name: string
+    bgColor: string
+    selectedColor: string
+  }>
+
+  @Column({ allowNull: false })
+    description!: string
+
+  @Column({ allowNull: false })
+    details!: Array<{
+    name: string
+    items: string[]
+  }>
+}
